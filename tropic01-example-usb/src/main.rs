be7ce8b76d05a57b91ef3e1222241bace01d8257
Sylvain Bellemare <sbellem@gmail.com>
2025-10-22 23:38:08 +0900

    wip: usb test main

--- tropic01-example-usb/src/main.rs
+++ tropic01-example-usb/src/main.rs
@@ -1,17 +1,37 @@
 #![allow(deprecated)] // Suppress aes-gcm warnings in tropic01
 
 use std::env;
+use std::fmt;
+use std::io;
 use std::thread;
-use std::time::Duration;
 use std::convert::TryInto;
+use std::error::Error;
+use std::time::Duration;
+use std::array::TryFromSliceError;
+
+use serialport;
+
+use ed25519_dalek::Signature;
+use ed25519_dalek::VerifyingKey;
 
 use embedded_hal::spi::{ErrorType, SpiDevice, Error as SpiError, ErrorKind};
+//use embedded_hal::spi::{Error as SpiError, ErrorKind};
+
+use rand_core::OsRng;
 use serialport::{DataBits, FlowControl, Parity, StopBits};
+use sha2::Digest as _;
+
 use tropic01::{Error as TropicError, Tropic01};
-use std::io;
+use tropic01::EccCurve;
+use tropic01::X25519Dalek;
+use tropic01::keys::SH0PRIV;
+use tropic01::keys::SH0PUB;
 
 use tropic01_example_usb::ChipId;
 use tropic01_example_usb::cert::Cert;
+
+use x25519_dalek::PublicKey;
+use x25519_dalek::StaticSecret;
 
 
 // Helper function for hex formatting
@@ -19,6 +39,110 @@ fn bytes_to_hex(bytes: &[u8]) -> String {
     bytes.iter().map(|b| format!("{:02x}", b)).collect::<Vec<_>>().join("")
 }
 
+/// XXX
+
+#[derive(Debug)]
+pub enum SerialTransportError {
+    Io(io::Error),
+    InvalidResponse,
+    DataTooLong,
+    NonUtf8Hex,
+    InvalidHexDigit,
+    InvalidBufferLength,
+    // Box is needed to break the recursion
+    Tropic(TropicError<Box<SerialTransportError>, std::convert::Infallible>),
+}
+
+impl fmt::Display for SerialTransportError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Io(err) => write!(f, "USB/Serial I/O error: {}", err),
+            Self::InvalidResponse => write!(f, "Invalid response from device"),
+            Self::DataTooLong => write!(f, "Data too long for transport"),
+            Self::NonUtf8Hex => write!(f, "Non-UTF8 hex characters in response"),
+            Self::InvalidHexDigit => write!(f, "Invalid hex digit in response"),
+            Self::InvalidBufferLength => write!(f, "Invalid buffer length"),
+            Self::Tropic(err) => write!(f, "Tropic device error: {}", err),
+        }
+    }
+}
+
+impl Error for SerialTransportError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            Self::Io(err) => Some(err),
+            Self::Tropic(err) => Some(err),
+            _ => None,
+        }
+    }
+}
+
+impl SpiError for SerialTransportError {
+    fn kind(&self) -> ErrorKind {
+        match self {
+            Self::Io(_) => ErrorKind::Other,
+            Self::InvalidResponse => ErrorKind::Other,
+            Self::DataTooLong => ErrorKind::Other,
+            Self::NonUtf8Hex => ErrorKind::Other,
+            Self::InvalidHexDigit => ErrorKind::Other,
+            Self::InvalidBufferLength => ErrorKind::Other,
+            Self::Tropic(_) => ErrorKind::Other,
+        }
+    }
+}
+
+impl From<io::Error> for SerialTransportError {
+    fn from(err: io::Error) -> Self {
+        Self::Io(err)
+    }
+}
+
+impl From<serialport::Error> for SerialTransportError {
+    fn from(err: serialport::Error) -> Self {
+        Self::Io(err.into())
+    }
+}
+
+impl From<TryFromSliceError> for SerialTransportError {
+    fn from(_err: TryFromSliceError) -> Self {
+        Self::InvalidBufferLength
+    }
+}
+
+impl From<TropicError<SerialTransportError, std::convert::Infallible>> for SerialTransportError {
+    fn from(err: TropicError<SerialTransportError, std::convert::Infallible>) -> Self {
+        match err {
+            // Special case for BusError which has a SerialTransportError inside
+            TropicError::BusError(inner) => Self::Tropic(
+                TropicError::BusError(Box::new(inner))
+            ),
+            // For variants that don't contain SerialTransportError, we can map directly
+            TropicError::AlarmMode => Self::Tropic(TropicError::AlarmMode),
+            TropicError::ChipBusy => Self::Tropic(TropicError::ChipBusy),
+            TropicError::Decryption(e) => Self::Tropic(TropicError::Decryption(e)),
+            TropicError::Encryption(e) => Self::Tropic(TropicError::Encryption(e)),
+            TropicError::GPIOError(_) => Self::InvalidResponse, // Infallible
+            TropicError::HandshakeFailed => Self::Tropic(TropicError::HandshakeFailed),
+            TropicError::InvalidChipStatus(e) => Self::Tropic(TropicError::InvalidChipStatus(e)),
+            TropicError::InvalidCRC => Self::Tropic(TropicError::InvalidCRC),
+            TropicError::InvalidKey => Self::Tropic(TropicError::InvalidKey),
+            TropicError::InvalidL2Response => Self::Tropic(TropicError::InvalidL2Response),
+            TropicError::InvalidL3Cmd => Self::Tropic(TropicError::InvalidL3Cmd),
+            TropicError::InvalidPublicKey => Self::Tropic(TropicError::InvalidPublicKey),
+            TropicError::L2ResponseError(e) => Self::Tropic(TropicError::L2ResponseError(e)),
+            TropicError::L3CmdFailed => Self::Tropic(TropicError::L3CmdFailed),
+            TropicError::L3ResponseBufferOverflow => Self::Tropic(TropicError::L3ResponseBufferOverflow),
+            TropicError::NoSession => Self::Tropic(TropicError::NoSession),
+            TropicError::ParsingError(e) => Self::Tropic(TropicError::ParsingError(e)),
+            TropicError::RequestExceedsSize => Self::Tropic(TropicError::RequestExceedsSize),
+            TropicError::Unauthorized => Self::Tropic(TropicError::Unauthorized),
+            TropicError::UnexpectedResponseStatus => Self::Tropic(TropicError::UnexpectedResponseStatus),
+        }
+    }
+}
+/// XXX
+
+/*
 #[derive(Debug)]
 pub enum SerialTransportError {
     Io(io::Error),
@@ -55,7 +179,8 @@ impl From<serialport::Error> for SerialTransportError {
         Self::Io(err.into())
     }
 }
-
+*/
+/*
 impl From<TropicError<SerialTransportError, std::convert::Infallible>> for SerialTransportError {
     fn from(err: TropicError<SerialTransportError, std::convert::Infallible>) -> Self {
         match err {
@@ -83,6 +208,7 @@ impl From<TropicError<SerialTransportError, std::convert::Infallible>> for Seria
         }
     }
 }
+*/
 
 pub struct SerialTransport {
     port: Box<dyn serialport::SerialPort>,
@@ -218,6 +344,7 @@ fn parse_and_print_cert(cert_bytes: &[u8]) {
 }
 
 
+/*
 fn main() -> Result<(), SerialTransportError> {
     env_logger::init();
 
@@ -299,5 +426,108 @@ fn main() -> Result<(), SerialTransportError> {
     /// XXX
 
     println!("Example completed successfully!");
+    Ok(())
+}
+*/
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    env_logger::init();
+    
+    let args: Vec<String> = env::args().collect();
+    let port_name = args.get(1).cloned().unwrap_or_else(|| "/dev/ttyACM0".to_string());
+
+    let baud_rate = args
+        .get(2)
+        .and_then(|s| s.parse().ok())
+        .filter(|&r| [4800, 9600, 19200, 38400, 115200].contains(&r))
+        .unwrap_or(115200);
+
+    println!("Opening TS1302 dongle on {} @ {} baud", port_name, baud_rate);
+
+    let transport = SerialTransport::new(&port_name, baud_rate)?;
+    let mut tropic01 = Tropic01::new(transport);
+
+
+
+    let res = tropic01.get_info_chip_id()?;
+    println!("ChipId: {res:x?}");
+    let chip_id = res.to_vec();
+
+    //println!("Sleep");
+    //tropic01.sleep_req(tropic01::SleepReq::Sleep)?;
+
+    let res = tropic01.get_info_cert()?;
+    println!("Cert: {res:x?}");
+
+    println!("Reboot");
+    tropic01.startup_req(tropic01::StartupReq::Reboot)?;
+    println!("Rebooted");
+
+    //let res = tropic01.get_info_chip_id()?;
+    //println!("ChipId after reboot: {res:x?}");
+    //assert_eq!(res, &chip_id);
+
+    let csprng = OsRng;
+    let ehpriv = StaticSecret::random_from_rng(csprng);
+    let ehpub = PublicKey::from(&ehpriv);
+    let shpub = SH0PUB.into();
+    let shpriv = SH0PRIV.into();
+    println!("ehpub: {:?}", ehpub);
+    println!("shpub: {:?}", shpub);
+    //println!("shpriv: {}", shpriv);
+    tropic01.session_start(&X25519Dalek, shpub, shpriv, ehpub, ehpriv, 0)?;
+
+    let res = tropic01.get_random_value(6)?;
+    println!("random value get: {res:x?}");
+
+    let ping_data = b"";
+    let res = tropic01.ping(ping_data)?;
+    // Test empty data loopback
+    assert_eq!(res, ping_data);
+
+    let ping_data = [6; 4096];
+    let res = tropic01.ping(&ping_data)?;
+    // Test long data loopback
+    assert_eq!(res, ping_data);
+
+    /*
+    let key_slot = 0.into();
+    tropic01.ecc_key_generate(key_slot, EccCurve::P256)?;
+
+    let res = tropic01.ecc_key_read(key_slot)?;
+    println!("key read response: {res:x?}");
+
+    let public_key =
+        VerifyingKey::from_bytes(res.pub_key().try_into()?).expect("public key to be valid");
+
+    // Signature of hash
+    let msg = "hello tropic";
+    let mut hasher = sha2::Sha256::new();
+    hasher.update(msg);
+    let hash: [u8; 32] = hasher.finalize().into();
+    let signature = tropic01.eddsa_sign(key_slot, &hash)?;
+    println!("signature of hash: {signature:x?}");
+    public_key
+        .verify_strict(&hash, &Signature::from_bytes(signature))
+        .expect("signature to be verified");
+
+    // Produce an unauthorized error to test nonce behavior
+    if shpub.as_bytes() == &SH0PUB {
+        assert!(matches!(
+            tropic01.ecc_key_generate(3.into(), EccCurve::P256),
+            Err(TropicError::Unauthorized)
+        ));
+    }
+
+    // Signature of raw message
+    let msg = "hello tropic".repeat(341);
+    let msg = msg.as_bytes();
+    let signature = tropic01.eddsa_sign(key_slot, msg)?;
+    println!("signature of long raw msg: {signature:x?}");
+    public_key
+        .verify_strict(msg, &Signature::from_bytes(signature))
+        .expect("signature to be verified");
+    */
     Ok(())
 }
