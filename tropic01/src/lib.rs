246ea9e4403c8f0e320827f58f70f638aa36ca86
Sylvain Bellemare <sbellem@gmail.com>
2025-11-13 14:10:23 +0900

    Add l3 commands for the monotonic counter

--- tropic01/src/lib.rs
+++ tropic01/src/lib.rs
@@ -28,6 +28,8 @@ pub use crate::lt_2::X509Certificate;
 pub use crate::lt_3::EccCurve;
 pub use crate::lt_3::EccKeyReadResponse;
 pub use crate::lt_3::EccOrigin;
+pub use crate::lt_3::MCOUNTER_VALUE_MAX;
+pub use crate::lt_3::MCounterIndex;
 
 pub mod common;
 mod crc16;
@@ -192,6 +194,10 @@ pub enum Error<ESpi, EGpio> {
     InvalidL3Cmd,
     #[display("Invalid public key in chip certificate")]
     InvalidPublicKey,
+    #[display("Invalid parameter")]
+    InvalidParameter,
+    #[display("Invalid response from chip")]
+    InvalidResponse,
     #[display("Error during processing of L2 cmd: {_0}")]
     L2ResponseError(ResponseStatus),
     #[display("Error during processing of L3 cmd")]
